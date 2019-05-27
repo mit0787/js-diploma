0cf82296489b90ebee67fdb6e22ffe8e84db1a3f
Dmitry <mit0787@gmail.com>
2019-05-27 22:24:14 +0300

    правка скрипта

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,27 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', function () {
+  //бургер-меню
+  let burgerMenu = document.querySelector('.burger-menu'),
+    btnBurger = document.querySelector('.burger');
+
+  document.addEventListener('click', (event) => {
+    if (screen.width <= 768) {
+      let target = event.target;
+      if (btnBurger.contains(event.target)) {
+        burgerMenu.classList.toggle('burger-menu_active');
+      } else if (!burgerMenu.contains(event.target)) {
+        burgerMenu.classList.remove('burger-menu_active');
+      }
+    }
+  });
+
+  window.addEventListener('resize', () => {
+    if (screen.width > 768) {
+      burgerMenu.classList.remove('burger-menu_active');
+    }
+  });
+
   // верхний слайдер
   let mainSlider = document.querySelectorAll('.main-slider-item'),
     currentSlide = 0,
@@ -20,6 +41,120 @@ window.addEventListener('DOMContentLoaded', function () {
     });
     slides[0].style.display = 'block';
   }
+
+  // подгрузка блоков
+  let styleBlock = document.querySelectorAll('.styles-2'),
+    btnStyles = document.querySelector('.button-styles');
+
+  btnStyles.addEventListener('click', function () {
+    this.style.display = 'none';
+    styleBlock.forEach((item) => {
+      item.style.display = 'block';
+    });
+  });
+
+  // калькулятор
+  let size = document.getElementById('size'),
+    material = document.getElementById('material'),
+    options = document.getElementById('options'),
+    promocode = document.querySelector('.promocode'),
+    price = document.querySelector('.calc-price'),
+    sizePrice = 0,
+    materialPrice = 0,
+    optionsPrice = 0,
+    promo = 0,
+    defaultValue = price.innerHTML;
+
+  size.addEventListener('change', function () {
+    sizePrice = +this.value;
+    let sum = (sizePrice + materialPrice + optionsPrice) * (100 - promo) / 100;
+    if (materialPrice > 0 && !isNaN(sizePrice) && !isNaN(materialPrice)) {
+      price.innerHTML = sum;
+    } else {
+      price.innerHTML = defaultValue;
+    }
+  });
+
+  material.addEventListener('change', function () {
+    materialPrice = +this.value;
+    let sum = (sizePrice + materialPrice + optionsPrice) * (100 - promo) / 100;
+    if (sizePrice > 0 && !isNaN(sizePrice) && !isNaN(materialPrice)) {
+      price.innerHTML = sum;
+    } else {
+      price.innerHTML = defaultValue;
+    }
+  });
+
+  options.addEventListener('change', function () {
+    optionsPrice = +this.value;
+    if (isNaN(optionsPrice)) {
+      optionsPrice = 0;
+    }
+    let sum = (sizePrice + materialPrice + optionsPrice) * (100 - promo) / 100;
+    if (sizePrice > 0 && materialPrice > 0) {
+      price.innerHTML = sum;
+    }
+  });
+
+  promocode.addEventListener('input', function () {
+    if (this.value === 'IWANTPOPART') {
+      promo = 30;
+    } else {
+      promo = 0;
+    }
+    let sum = (sizePrice + materialPrice + optionsPrice) * (100 - promo) / 100;
+    if (sizePrice > 0 && materialPrice > 0) {
+      price.innerHTML = sum;
+    }
+  });
+
+  // табы
+  let tab = document.querySelectorAll('.portfolio-menu li'),
+    portfolio = document.querySelector('.portfolio-menu'),
+    tabContent = document.querySelectorAll('.portfolio-block'),
+    noPortfolio = document.querySelector('.portfolio-no');
+
+  portfolio.addEventListener('click', (event) => {
+    let target = event.target,
+      tabClass = target.classList[0];
+    noPortfolio.style.display = 'block';
+    tab.forEach((item) => {
+      item.classList.remove('active');
+    });
+    target.classList.add('active');
+    tabContent.forEach((item) => {
+      item.style.display = 'none';
+      if (item.classList.contains(tabClass)) {
+        item.style.display = 'block';
+        noPortfolio.style.display = 'none';
+      }
+    });
+  });
+
+  // смена картинок при наведении или тапе
+  let imageBlock = document.querySelectorAll('.sizes-block');
+
+  document.addEventListener('mouseover', (event) => {
+    let target = event.target;
+    imageBlock.forEach((item, i) => {
+      let image = item.querySelector('img[class^="size"]');
+      let text = item.querySelectorAll('p');
+      if (item.contains(event.target)) {
+        image.src = `./img/sizes-${i+1}-1.png`;
+        text.forEach((item) => {
+          item.style.display = 'none';
+          if (item.classList.contains('sizes-hit')) {
+            item.style.display = 'block';
+          }
+        });
+      } else {
+        image.src = `./img/sizes-${i+1}.png`;
+        text.forEach((item) => {
+          item.style.display = 'block';
+        });
+      }
+    });
+  });
 
   // нижний слайдер
   let feedbackSlider = document.querySelectorAll('.feedback-slider-item'),
@@ -63,88 +198,30 @@ window.addEventListener('DOMContentLoaded', function () {
     plusSlides(1);
   });
 
-  // закрытие модальных окон
-  let btnClose = document.querySelectorAll('.popup-close');
+  //аккордеон
+  let btnAccord = document.querySelectorAll('.accordion-heading'),
+    blockAccord = document.querySelectorAll('.accordion-block');
 
-  btnClose.forEach((item) => {
-    let modal = item.parentNode.parentNode.parentNode;
+  blockAccord.forEach((item) => {
+    item.style.display = 'none';
+  });
 
+  btnAccord.forEach((item, i) => {
     item.addEventListener('click', () => {
-      closePopup();
-    });
-
-    window.addEventListener('click', (event) => {
-      if (event.target === modal) {
-        closePopup();
+      if (blockAccord[i].style.display === 'none') {
+        btnAccord.forEach((item, i) => {
+          item.classList.remove('active');
+        });
+        blockAccord.forEach((item) => {
+          item.style.display = 'none';
+        });
+        item.classList.add('active');
+        blockAccord[i].style.display = 'block';
+      } else {
+        blockAccord[i].style.display = 'none';
       }
     });
-
-    function closePopup() {
-      modal.style.display = 'none';
-      document.body.style.overflow = '';
-    }
   });
-
-  // модальное окно заказа
-  let btnsDesign = document.querySelectorAll('.button-design'),
-    popupDesign = document.querySelector('.popup-design');
-
-  btnsDesign.forEach((item) => {
-    item.addEventListener('click', () => {
-      openModal(popupDesign);
-    });
-  });
-
-  // модальное окно консультации с открытием по таймеру
-  let btnsConsult = document.querySelectorAll('.button-consultation'),
-    popupConsult = document.querySelector('.popup-consultation'),
-    consultTimer = setTimeout(openConsult, 60000);
-
-  btnsConsult.forEach((item) => {
-    item.addEventListener('click', () => {
-      openModal(popupConsult);
-    });
-  });
-
-  function openConsult() {
-    openModal(popupConsult);
-  }
-
-  // модальное окно подарка с открытием при прокрутке вниз страницы
-  let btnsGift = document.querySelector('.fixed-gift'),
-    popupGift = document.querySelector('.popup-gift');
-
-  btnsGift.addEventListener('click', () => {
-    openModal(popupGift);
-    btnsGift.style.display = 'none';
-  });
-
-  let height = document.body.getBoundingClientRect().height,
-    timeout = setTimeout(() => {
-      if (window.pageYOffset < screen.height) {
-        window.addEventListener('scroll', openGift);
-      }
-    }, 1000);
-
-  function openGift() {
-    if (window.pageYOffset + window.outerHeight > height) {
-      openModal(popupGift);
-      btnsGift.style.display = 'none';
-      window.removeEventListener('scroll', openGift);
-    }
-  }
-
-  document.addEventListener('click', (event) => {
-    if (event.target.classList.contains('button')) {
-      window.removeEventListener('scroll', openGift);
-    }
-  });
-
-  function openModal(modal) {
-    modal.style.display = 'block';
-    clearTimeout(consultTimer);
-    document.body.style.overflow = 'hidden';
-  }
 
   // формы
   let mainForm = document.getElementById('main-form'),
@@ -282,163 +359,86 @@ window.addEventListener('DOMContentLoaded', function () {
     }
   }
 
-  //бургер-меню
-  let burgerMenu = document.querySelector('.burger-menu'),
-    btnBurger = document.querySelector('.burger');
+  // закрытие модальных окон
+  let btnClose = document.querySelectorAll('.popup-close');
+
+  btnClose.forEach((item) => {
+    let modal = item.parentNode.parentNode.parentNode;
+
+    item.addEventListener('click', () => {
+      closePopup();
+    });
+
+    window.addEventListener('click', (event) => {
+      if (event.target === modal) {
+        closePopup();
+      }
+    });
+
+    function closePopup() {
+      modal.style.display = 'none';
+      document.body.style.overflow = '';
+    }
+  });
+
+  // модальное окно заказа
+  let btnsDesign = document.querySelectorAll('.button-design'),
+    popupDesign = document.querySelector('.popup-design');
+
+  btnsDesign.forEach((item) => {
+    item.addEventListener('click', () => {
+      openModal(popupDesign);
+    });
+  });
+
+  // модальное окно консультации с открытием по таймеру
+  let btnsConsult = document.querySelectorAll('.button-consultation'),
+    popupConsult = document.querySelector('.popup-consultation'),
+    consultTimer = setTimeout(openConsult, 60000);
+
+  btnsConsult.forEach((item) => {
+    item.addEventListener('click', () => {
+      openModal(popupConsult);
+    });
+  });
+
+  function openConsult() {
+    openModal(popupConsult);
+  }
+
+  // модальное окно подарка с открытием при прокрутке вниз страницы
+  let btnsGift = document.querySelector('.fixed-gift'),
+    popupGift = document.querySelector('.popup-gift');
+
+  btnsGift.addEventListener('click', () => {
+    openModal(popupGift);
+    btnsGift.style.display = 'none';
+  });
+
+  let height = document.body.getBoundingClientRect().height,
+    timeout = setTimeout(() => {
+      if (window.pageYOffset < screen.height) {
+        window.addEventListener('scroll', openGift);
+      }
+    }, 1000);
+
+  function openGift() {
+    if (window.pageYOffset + window.outerHeight > height) {
+      openModal(popupGift);
+      btnsGift.style.display = 'none';
+      window.removeEventListener('scroll', openGift);
+    }
+  }
 
   document.addEventListener('click', (event) => {
-    if (screen.width <= 768) {
-      let target = event.target;
-      if (btnBurger.contains(event.target)) {
-        burgerMenu.classList.toggle('burger-menu_active');
-      } else if (!burgerMenu.contains(event.target)) {
-        burgerMenu.classList.remove('burger-menu_active');
-      }
+    if (event.target.classList.contains('button')) {
+      window.removeEventListener('scroll', openGift);
     }
   });
 
-  window.addEventListener('resize', () => {
-    if (screen.width > 768) {
-      burgerMenu.classList.remove('burger-menu_active');
-    }
-  });
-
-  //аккордеон
-  let btnAccord = document.querySelectorAll('.accordion-heading'),
-    blockAccord = document.querySelectorAll('.accordion-block');
-
-  blockAccord.forEach((item) => {
-    item.style.display = 'none';
-  });
-
-  btnAccord.forEach((item, i) => {
-    item.addEventListener('click', () => {
-      if (blockAccord[i].style.display === 'none') {
-        btnAccord.forEach((item, i) => {
-          item.classList.remove('active');
-        });
-        blockAccord.forEach((item) => {
-          item.style.display = 'none';
-        });
-        item.classList.add('active');
-        blockAccord[i].style.display = 'block';
-      } else {
-        blockAccord[i].style.display = 'none';
-      }
-    });
-  });
-
-  // смена картинок при наведении или тапе
-  let imageBlock = document.querySelectorAll('.sizes-block');
-
-  document.addEventListener('mouseover', (event) => {
-    let target = event.target;
-    imageBlock.forEach((item, i) => {
-      let image = item.querySelector('img[class^="size"]');
-      let text = item.querySelectorAll('p');
-      if (item.contains(event.target)) {
-        image.src = `./img/sizes-${i+1}-1.png`;
-        text.forEach((item) => {
-          item.style.display = 'none';
-          if (item.classList.contains('sizes-hit')) {
-            item.style.display = 'block';
-          }
-        });
-      } else {
-        image.src = `./img/sizes-${i+1}.png`;
-        text.forEach((item) => {
-          item.style.display = 'block';
-        });
-      }
-    });
-  });
-
-  // табы
-  let tab = document.querySelectorAll('.portfolio-menu li'),
-    portfolio = document.querySelector('.portfolio-menu'),
-    tabContent = document.querySelectorAll('.portfolio-block'),
-    noPortfolio = document.querySelector('.portfolio-no');
-
-  portfolio.addEventListener('click', (event) => {
-    let target = event.target,
-      tabClass = target.classList[0];
-    noPortfolio.style.display = 'block';
-    tab.forEach((item) => {
-      item.classList.remove('active');
-    });
-    target.classList.add('active');
-    tabContent.forEach((item) => {
-      item.style.display = 'none';
-      if (item.classList.contains(tabClass)) {
-        item.style.display = 'block';
-        noPortfolio.style.display = 'none';
-      }
-    });
-  });
-
-  // калькулятор
-  let size = document.getElementById('size'),
-    material = document.getElementById('material'),
-    options = document.getElementById('options'),
-    promocode = document.querySelector('.promocode'),
-    price = document.querySelector('.calc-price'),
-    sizePrice = 0,
-    materialPrice = 0,
-    optionsPrice = 0,
-    promo = 0,
-    defaultValue = price.innerHTML;
-
-  size.addEventListener('change', function () {
-    sizePrice = +this.value;
-    let sum = (sizePrice + materialPrice + optionsPrice) * (100 - promo) / 100;
-    if (materialPrice > 0 && !isNaN(sizePrice) && !isNaN(materialPrice)) {
-      price.innerHTML = sum;
-    } else {
-      price.innerHTML = defaultValue;
-    }
-  });
-
-  material.addEventListener('change', function () {
-    materialPrice = +this.value;
-    let sum = (sizePrice + materialPrice + optionsPrice) * (100 - promo) / 100;
-    if (sizePrice > 0 && !isNaN(sizePrice) && !isNaN(materialPrice)) {
-      price.innerHTML = sum;
-    } else {
-      price.innerHTML = defaultValue;
-    }
-  });
-
-  options.addEventListener('change', function () {
-    optionsPrice = +this.value;
-    if (isNaN(optionsPrice)) {
-      optionsPrice = 0;
-    }
-    let sum = (sizePrice + materialPrice + optionsPrice) * (100 - promo) / 100;
-    if (sizePrice > 0 && materialPrice > 0) {
-      price.innerHTML = sum;
-    }
-  });
-
-  promocode.addEventListener('input', function () {
-    if (this.value === 'IWANTPOPART') {
-      promo = 30;
-    } else {
-      promo = 0;
-    }
-    let sum = (sizePrice + materialPrice + optionsPrice) * (100 - promo) / 100;
-    if (sizePrice > 0 && materialPrice > 0) {
-      price.innerHTML = sum;
-    }
-  });
-
-  // подгрузка блоков
-  let styleBlock = document.querySelectorAll('.styles-2'),
-    btnStyles = document.querySelector('.button-styles');
-
-  btnStyles.addEventListener('click', function () {
-    this.style.display = 'none';
-    styleBlock.forEach((item) => {
-      item.style.display = 'block';
-    });
-  });
+  function openModal(modal) {
+    modal.style.display = 'block';
+    clearTimeout(consultTimer);
+    document.body.style.overflow = 'hidden';
+  }
 });
